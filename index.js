9b5ecb909794301d6452382f0d1120f855f9a43a
bob <sammy.chesire@student.moringaschool.com>
2022-08-27 18:26:34 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  return word === word.split("").reverse().join("");
+  
+   
+  console.log("Is Palindrome? : " + isPalindrome(word));
+  console.log("Is Palindrome?: " + isPalindrome(word));
+  
 }
 
 /* 
   Add your pseudocode here
 */
-
+// split the String
+//  reverse the array
+//  join the string
 /*
   Add written explanation of your solution here
-*/
-
+ */
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
